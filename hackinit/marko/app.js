6d42479c58015aed7d0d58f6ae5d1d47299d7b79
yuchong-pan <panyuchong@gmail.com>
2018-01-28 00:04:01 -0800

    Added coming-soon route in app.js

--- hackinit/marko/app.js
+++ hackinit/marko/app.js
@@ -4,4 +4,6 @@ require("marko/node-require").install();
 var express = require("express");
 var app = express();
 
+app.get(["/:lang", "*"], require("./src/pages/coming-soon"));
+
 app.listen(8080);
