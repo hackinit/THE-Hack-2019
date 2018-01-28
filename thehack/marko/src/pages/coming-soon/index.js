2d1e1f073b77895286e74f3efcb345a194fe37b6
yuchong-pan <panyuchong@gmail.com>
2018-01-28 04:01:36 -0800

    Added marko containers for THE Hack

---
+++ thehack/marko/src/pages/coming-soon/index.js
@@ -0,0 +1,27 @@
+var dataAll = require("./langs.json");
+var template = require("./template.marko");
+
+var langs = ["zh"];
+
+function getLang(req, res) {
+    if (req.params.lang && langs.indexOf(req.params.lang) != -1) {
+        return req.params.lang;
+    } else if (req.path === "/") {
+        return "en";
+    } else {
+        res.redirect("/");
+    }
+}
+
+module.exports = function(req, res) {
+    var lang = getLang(req, res);
+    var data = {};
+    for (var key in dataAll) {
+        if (dataAll[key][lang] !== undefined) {
+            data[key] = dataAll[key][lang];
+        } else {
+            data[key] = dataAll[key]["default"];
+        }
+    }
+    res.marko(template, data);
+};
