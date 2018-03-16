d6497323d55c9d3ecae1df748aac84a4da7cd909
yuchong-pan <panyuchong@gmail.com>
2018-03-16 16:17:18 -0700

    Added scripts for processing multi-language

--- hackinit/marko/src/pages/coming-soon/index.js
+++ hackinit/marko/src/pages/coming-soon/index.js
@@ -1,27 +1,7 @@
 var dataAll = require("./langs.json");
 var template = require("./template.marko");
-
-var langs = ["zh"];
-
-function getLang(req, res) {
-    if (req.params.lang && langs.indexOf(req.params.lang) != -1) {
-        return req.params.lang;
-    } else if (req.path === "/") {
-        return "en";
-    } else {
-        res.redirect("/");
-    }
-}
+var render = require("./../../scripts/render.js");
 
 module.exports = function(req, res) {
-    var lang = getLang(req, res);
-    var data = {};
-    for (var key in dataAll) {
-        if (dataAll[key][lang] !== undefined) {
-            data[key] = dataAll[key][lang];
-        } else {
-            data[key] = dataAll[key]["default"];
-        }
-    }
-    res.marko(template, data);
+    render(req, res, dataAll, template);
 };
