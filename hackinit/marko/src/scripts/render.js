27cfe1e692c67bb9fc7afeadf852fe24c7800595
yuchong-pan <panyuchong@gmail.com>
2018-03-16 16:17:18 -0700

    Added scripts for processing multi-language

---
+++ hackinit/marko/src/scripts/render.js
@@ -0,0 +1,30 @@
+var langs = require("./langs.json")
+
+function getLang(req, res) {
+    if (req.params.lang && langs.supported.indexOf(req.params.lang) != -1) {
+        return req.params.lang;
+    } else if (req.path === "/") {
+        return langs.default;
+    } else {
+        res.redirect("/");
+        return null;
+    }
+}
+
+function render(req, res, dataAll, template) {
+    var lang = getLang(req);
+    if (lang === null) {
+        return;
+    }
+    var data = {};
+    for (var key in dataAll) {
+        if (dataAll[key][lang] !== undefined) {
+            data[key] = dataAll[key][lang];
+        } else {
+            data[key] = dataAll[key]["default"];
+        }
+    }
+    res.marko(template, data);
+}
+
+module.exports = render;
--- thehack/marko/src/pages/coming-soon/index.js
+++ thehack/marko/src/pages/coming-soon/index.js
@@ -1,31 +1,7 @@
 var dataAll = require("./langs.json");
 var template = require("./template.marko");
-
-var langs = ["zh"];
-
-function getLang(req, res) {
-    if (req.params.lang && langs.indexOf(req.params.lang) != -1) {
-        return req.params.lang;
-    } else if (req.path === "/") {
-        return "en";
-    } else {
-        res.redirect("/");
-        return null;
-    }
-}
+var render = require("./../../scripts/render.js");
 
 module.exports = function(req, res) {
-    var lang = getLang(req, res);
-    if (lang === null) {
-        return;
-    }
-    var data = {};
-    for (var key in dataAll) {
-        if (dataAll[key][lang] !== undefined) {
-            data[key] = dataAll[key][lang];
-        } else {
-            data[key] = dataAll[key]["default"];
-        }
-    }
-    res.marko(template, data);
+    render(req, res, dataAll, template);
 };
