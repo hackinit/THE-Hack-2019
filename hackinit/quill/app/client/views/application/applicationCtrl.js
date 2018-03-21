67a43a4c64759ec07aaf815d31af0e5349064a41
Shaolin Zhang <ShaolinZhang@users.noreply.github.com>
2018-03-21 15:11:31 +0800

    Updated s3

--- hackinit/quill/app/client/views/application/applicationCtrl.js
+++ hackinit/quill/app/client/views/application/applicationCtrl.js
@@ -17,7 +17,7 @@ angular.module('reg')
         url: '/api/resume/upload',
         previewTemplate: document.querySelector('#resume-dropzone-preview').innerHTML,
         maxFiles: 1,
-        maxFilesize: .5, // MB
+        maxFilesize: 2, // MB
         uploadMultiple: false,
         acceptedFiles: 'application/pdf',
         autoProcessQueue: false,
