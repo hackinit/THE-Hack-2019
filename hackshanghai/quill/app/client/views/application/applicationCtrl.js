e4d5cddf4a99d92d2489753f503285545034bf47
Shaolin Zhang <ShaolinZhang@users.noreply.github.com>
2018-04-19 08:33:40 -0700

    Added questions

--- hackshanghai/quill/app/client/views/application/applicationCtrl.js
+++ hackshanghai/quill/app/client/views/application/applicationCtrl.js
@@ -8,45 +8,65 @@ angular.module('reg')
     'settings',
     'Session',
     'UserService',
-    function($scope, $rootScope, $state, $http, currentUser, Settings, Session, UserService){
+    'AutocompleteService',
+    function($scope, $rootScope, $state, $http, currentUser, Settings, Session, UserService, AutocompleteService){
 
       // Set up the user
       $scope.user = currentUser.data;
+      $scope.travelFormId = $scope.user._id.substr($scope.user._id.length - 7);
 
-      // Is the student from MIT?
-      $scope.isMitStudent = $scope.user.email.split('@')[1] == 'mit.edu';
-
-      $scope.user.profile.adult = true;
-
-      // If so, default them to adult: true
-      if ($scope.isMitStudent){
-        $scope.user.profile.adult = true;
+      var currentYear = new Date().getFullYear();
+      $scope.graduationYears = [];
+      for (var i = -1; i <= 10; i++) {
+        $scope.graduationYears.push(currentYear + i);
       }
+
+
+      // <tracks>
+      var tracks = [
+        "Digital Journalism",
+        "Security",
+        "Smart Society",
+        "E-Health",
+        "Free Choice",
+      ];
+
+      $scope.tracks = {};
+
+      for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+
+        $scope.tracks[track] = $scope.user.profile.ideaTracks.indexOf(track) != -1;
+      }
+      // </tracks>
+
 
       // Populate the school dropdown
       populateSchools();
+      populateNations();
       _setupForm();
 
-      $scope.regIsClosed = Date.now() > Settings.data.timeClose;
+      $scope.regIsClosed = Date.now() > Settings.data.timeClose || ($scope.user.status && $scope.user.status.admitted);
 
       /**
        * TODO: JANK WARNING
        */
       function populateSchools(){
-        $http
-          .get('/assets/schools.json')
+        AutocompleteService
+          .getSchoolDomains()
           .then(function(res){
             var schools = res.data;
             var email = $scope.user.email.split('@')[1];
 
             if (schools[email]){
-              $scope.user.profile.school = schools[email].school;
+              $scope.user.profile.study = $scope.user.profile.study || {};
+              $scope.user.profile.study.school = schools[email].school;
               $scope.autoFilledSchool = true;
             }
           });
 
-        $http
-          .get('/assets/schools.csv')
+        AutocompleteService
+          .getOtherSchools()
           .then(function(res){
             $scope.schools = res.data.split('\n');
             $scope.schools.push('Other');
@@ -63,19 +83,39 @@ angular.module('reg')
                 source: content,
                 cache: true,
                 onSelect: function(result, response) {
-                  $scope.user.profile.school = result.title.trim();
+                  $scope.user.profile.study.school = result.title.trim();
                 }
               })
           });
       }
 
+      function populateNations() {
+        AutocompleteService
+          .getNationalities()
+          .then(function(res){
+            $scope.nations = res.data;
+          });
+      }
+
       function _updateUser(e){
+        // <tracks>
+        $scope.user.profile.ideaTracks = [
+          "Digital Journalism",
+          "Security",
+          "Smart Society",
+          "E-Health",
+          "Free Choice",
+        ].filter(function (track) {
+          return $scope.tracks[track];
+        });
+        // </tracks>
+
         UserService
           .updateProfile(Session.getUserId(), $scope.user.profile)
           .success(function(data){
             sweetAlert({
-              title: "谢谢！",
-              text: "你的申请已被成功保存",
+              title: "Awesome!",
+              text: "Your application has been saved.",
               type: "success",
               confirmButtonColor: "#e76482"
             }, function(){
@@ -83,59 +123,64 @@ angular.module('reg')
             });
           })
           .error(function(res){
-            sweetAlert("Oops", "可能有哪里出错了", "error");
+            sweetAlert("Uh oh!", "Something went wrong.", "error");
           });
       }
 
-      function isMinor() {
-        return !$scope.user.profile.adult;
-      }
-
-      function minorsAreAllowed() {
-        return Settings.data.allowMinors;
-      }
-
-      function minorsValidation() {
-        // Are minors allowed to register?
-        if (isMinor() && !minorsAreAllowed()) {
-          return false;
-        }
-        return true;
-      }
-
       function _setupForm(){
-        // Custom minors validation rule
-        $.fn.form.settings.rules.allowMinors = function (value) {
-          return minorsValidation();
+        $.fn.form.settings.rules.professionSelected = function(value) {
+          var profession = $("input[name='profession']:checked").val();
+
+          return profession == "W" || profession == "S";
+        };
+
+        $.fn.form.settings.rules.schoolSelectedAndEmpty = function(value) {
+          var profession = $("input[name='profession']:checked").val();
+
+          return (profession == "S" && value.length > 0) || profession == "W";
+        };
+
+        $.fn.form.settings.rules.workSelectedAndEmpty = function(value) {
+          var profession = $("input[name='profession']:checked").val();
+
+          return (profession == "W" && value.length > 0) || profession == "S";
+        };
+
+        $.fn.form.settings.rules.workSelectedAndIntegerBetween1And100 = function(value) {
+          var profession = $("input[name='profession']:checked").val();
+
+          return (profession == "W" && value >= 1 && value <= 100) || profession == "S";
+        };
+
+        $.fn.form.settings.rules.travelReimbursementSelected = function(value) {
+          var travelReimbursement = $("input[name='travel-reimbursement']:checked").val();
+
+          return travelReimbursement == "Y" || travelReimbursement == "N";
+        };
+
+        $.fn.form.settings.rules.travelReimbursementAndTypeProvided = function(value) {
+          return ($scope.user.profile.travelReimbursement == "Y" && $scope.user.profile.travelReimbursementType) || $scope.user.profile.travelReimbursement == "N";
         };
 
         // Semantic-UI form validation
         $('.ui.form').form({
+          inline: true,
           fields: {
             name: {
               identifier: 'name',
               rules: [
                 {
                   type: 'empty',
-                  prompt: '请输入你的姓名'
+                  prompt: '请填写你的法定全名'
                 }
               ]
             },
-            school: {
-              identifier: 'school',
+            age: {
+              identifier: 'age',
               rules: [
                 {
-                  type: 'empty',
-                  prompt: '请输入你的学校全称'
-                }
-              ]
-            },
-            year: {
-              identifier: 'year',
-              rules: [
-                {
-                  type: 'empty',
-                  prompt: '请选择你的毕业年份'
+                  type: 'integer[1..150]',
+                  prompt: '请输入你的年龄'
                 }
               ]
             },
@@ -144,7 +189,155 @@ angular.module('reg')
               rules: [
                 {
                   type: 'empty',
+                  prompt: '请选择你的性别'
+                }
+              ]
+            },
+            phoneNum: {
+              identifier: 'phoneNum',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: '请输入你的手机号码'
+                }
+              ]
+            },
+            city: {
+              identifier: 'city',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: '请输入你的所在城市'
+                }
+              ]
+            },
+
+            profession: {
+              identifier: 'profession',
+              rules: [
+                {
+                  type: 'professionSelected',
+                  prompt: '请选择你的身份'
+                }
+              ]
+            },
+
+            school: {
+              identifier: 'school',
+              rules: [
+                {
+                  type: 'schoolSelectedAndEmpty',
+                  prompt: '请输入你的学校名称'
+                }
+              ]
+            },
+
+            subjectOfStudy: {
+              identifier: 'subject-of-study',
+              rules: [
+                {
+                  type: 'schoolSelectedAndEmpty',
+                  prompt: '请输入你的专业'
+                }
+              ]
+            },
+
+            yearOfStudies: {
+              identifier: 'year-of-studies',
+              rules: [
+                {
+                  type: 'schoolSelectedAndEmpty',
+                  prompt: '请输入你的就读时长'
+                }
+              ]
+            },
+
+            graduationYear: {
+              identifier: 'graduation-year',
+              rules: [
+                {
+                  type: 'schoolSelectedAndEmpty',
                   prompt: '请选择你的毕业年份'
+                }
+              ]
+            },
+
+            workExperience: {
+              identifier: 'work-experience',
+              rules: [
+                {
+                  type: 'workSelectedAndIntegerBetween1And100',
+                  prompt: '请输入你的工作经验'
+                }
+              ]
+            },
+
+            travelReimbursement: {
+              identifier: 'travel-reimbursement',
+              rules: [
+                {
+                  type: 'travelReimbursementSelected',
+                  prompt: '请选择你是否需要交通补助'
+                }
+              ]
+            },
+
+            travelReimbursementType: {
+              identifier: 'travel-reimbursement-type',
+              rules: [
+                {
+                  type: 'travelReimbursementAndTypeProvided',
+                  prompt: '请选择你的出发区域'
+                }
+              ]
+            },
+
+            description: {
+              identifier: 'description',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: '请选择你的团队角色'
+                }
+              ]
+            },
+
+            interested-field: {
+              identifier: 'interested-field',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: '请输入你感兴趣的产业'
+                }
+              ]
+            },
+
+            idea: {
+              identifier: 'idea',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: '请选择你的项目想法'
+                }
+              ]
+            },
+
+            cocRead: {
+              identifier: 'coc-read',
+              rules: [
+                {
+                  type: 'checked',
+                  prompt: '请阅读并同意《选手行为守则》'
+                }
+              ]
+            },
+
+            mlhTerms: {
+              identifier: 'mlh-terms',
+              rules: [
+                {
+                  type: 'checked',
+                  prompt: 'Please accept the MLH terms.'
                 }
               ]
             },
@@ -157,6 +350,9 @@ angular.module('reg')
       $scope.submitForm = function(){
         if ($('.ui.form').form('is valid')){
           _updateUser();
+        }
+        else{
+          sweetAlert("Uh oh!", "Please Fill The Required Fields", "error");
         }
       };
 
