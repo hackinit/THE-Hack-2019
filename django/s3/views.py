93ccd83712b33fded4e8a5082ab92c25eae30651
yuchong-pan <panyuchong@gmail.com>
2018-04-18 17:15:21 -0700

    Added publishing for asynchronous S3 middleware

--- django/s3/views.py
+++ django/s3/views.py
@@ -4,31 +4,38 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 
 import boto3
-import StringIO
+import redis
+import random
+
+ALPHABET = [chr(ord('0') + i) for i in range(10)] \
+    + [chr(ord('a') + i) for i in range(26)] \
+    + [chr(ord('A') + i) for i in range(26)]
 
 class S3Middleware(APIView):
     parser_classes = (MultiPartParser, FormParser,)
 
     def put(self, request, filename, format=None):
-        try:
-            s3 = boto3.resource('s3')
-            upload = request.FILES['upload']
-            content = ''
-            for chunk in upload.chunks():
-                content += chunk
-            output = StringIO.StringIO()
-            output.write(content)
-            output.seek(0)
-            s3.Bucket('thehack').put_object(Key='upload/{}'.format(filename), Body=output)
-            return Response({
-                'status': 204,
-                'message': 'upload success',
-            }, status=204)
-        except Exception as e:
-            print 'ERROR: {}'.format(e)
-            return Response({
-                'status': 400,
-                'message': 'upload failed',
-            }, status=400)
-        finally:
-            output.close()
+        content = self.__extract_file(request)
+
+        db_filename = redis.StrictRedis(host='redis', port=6379, db=0)
+        db_content = redis.StrictRedis(host='redis', port=6379, db=1)
+        db_queue = redis.StrictRedis(host='redis', port=6379, db=2)
+
+        random.shuffle(ALPHABET)
+        token = ''.join(ALPHABET[:8])
+        db_filename.set(token, filename)
+        db_content.set(token, content)
+
+        db_queue.publish('upload-channel', token)
+
+        return Response({
+            'token': token,
+            'status': 200,
+        })
+
+    def __extract_file(self, request):
+        upload = request.FILES['upload']
+        content = ''
+        for chunk in upload.chunks():
+            content += chunk
+        return content
