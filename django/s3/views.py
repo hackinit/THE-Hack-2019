e88c1e5b1eaec0ef0a1545f41ed0fc645618717f
yuchong-pan <panyuchong@gmail.com>
2018-04-18 17:56:35 -0700

    Added upload processing container for S3 middleware

--- django/s3/views.py
+++ django/s3/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 
-import boto3
 import redis
 import random
 
---
+++ s3/listen.py
@@ -0,0 +1,37 @@
+import boto3
+import redis
+import StringIO
+from multiprocessing import Process
+
+def s3(filename, file_obj):
+    s3 = boto3.resource('s3')
+    s3.Bucket('thehack').put_object(Key='upload/{}'.format(filename), Body=file_obj)
+
+def upload(filename, file_obj):
+    db_queue = redis.StrictRedis(host='localhost', port=6379, db=2)
+    try:
+        s3(filename, file_obj)
+        db_queue.publish("result-success", token)
+    except Exception:
+        db_queue.publish("result-failed", token)
+    finally:
+        file_obj.close()
+
+if __name__ == '__main__':
+    db_filename = redis.StrictRedis(host='redis', port=6379, db=0)
+    db_content = redis.StrictRedis(host='redis', port=6379, db=1)
+    db_queue = redis.StrictRedis(host='redis', port=6379, db=2)
+    p = db_queue.pubsub()
+    p.subscribe("upload-channel")
+    for message in p.listen():
+        token = message['data']
+        if db_filename.get(token) is None:
+            continue
+        filename = db_filename.get(token)
+        content = db_content.get(token)
+
+        file_obj = StringIO.StringIO(content)
+        file_obj.seek(0)
+
+        p = Process(target=upload, args=(filename, file_obj))
+        p.start()
