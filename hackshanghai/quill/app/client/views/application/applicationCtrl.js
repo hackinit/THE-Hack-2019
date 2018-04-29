889bc14706c3fdaae4071144c46b2c1fad448927
Shaolin Zhang <ShaolinZhang@users.noreply.github.com>
2018-04-29 11:24:21 +0800

    Changed thehack

--- hackshanghai/quill/app/client/views/application/applicationCtrl.js
+++ hackshanghai/quill/app/client/views/application/applicationCtrl.js
@@ -376,7 +376,7 @@ angular.module('reg')
           formData.append('upload', resume, resume.name);
           $.ajax({
             type: 'PUT',
-            url: 'http://api.thehack.io/s3/upload/resume/' + $scope.user._id + '_resume' + _getExtension(resume.name),
+            url: 'http://api.thehack.org.cn/s3/upload/resume/' + $scope.user._id + '_resume' + _getExtension(resume.name),
             data: formData,
             processData: false,
             contentType: false,
