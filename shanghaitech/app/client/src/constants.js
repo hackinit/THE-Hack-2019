25e92cf83aff984919be3322f7ccb72577e0e190
yuchong-pan <panyuchong@gmail.com>
2018-05-21 17:55:33 -0700

    Added a Quill container for ShanghaiTech students

---
+++ shanghaitech/app/client/src/constants.js
@@ -0,0 +1,21 @@
+angular.module('reg')
+    .constant('EVENT_INFO', {
+        NAME: 'hackShanghai 2018',
+    })
+    .constant('DASHBOARD', {
+        UNVERIFIED: '我们已经向你的注册邮箱发送了验证邮件。完成验证后，申请通道将开放。',
+        INCOMPLETE_TITLE: '你的申请仍未完成',
+        INCOMPLETE: '如果你在 [APP_DEADLINE] 前无法完成申请，你将无法被组委会审查参赛资格。请尽快完成你的申请。',
+        SUBMITTED_TITLE: '你的申请已提交',
+        SUBMITTED: '我们欢迎你随时修改你的申请。但是，一旦申请结束，你将无法进一步编辑你的申请内容。\n 我们的审核将完全基于你提交的信息，请确保你提交的所有信息真实有效。',
+        CLOSED_AND_INCOMPLETE_TITLE: '很抱歉，申请已经结束。',
+        CLOSED_AND_INCOMPLETE: 'Because you have not completed your profile in time, you will not be eligible for the lottery process.',
+        ADMITTED_AND_CAN_CONFIRM_TITLE: '请在 [CONFIRM_DEADLINE] 前确认参赛',
+        ADMITTED_AND_CANNOT_CONFIRM_TITLE: '你的参赛邀请已经失效',
+        ADMITTED_AND_CANNOT_CONFIRM: '虽然我们为你提供了参赛资格，但是你并没有在参赛确认截止日期前确认。\n很遗憾，你的邀请已经提供给了等待名单上的其他申请者。\n希望在明年的活动宣传中见到你！',
+        CONFIRMED_NOT_PAST_TITLE: '你可以在 [CONFIRM_DEADLINE] 前修改你的确认信息。',
+        DECLINED: '很遗憾，我们今年无法邀请你参加我们的参赛计划 :(\n希望在明年的活动中见到你！',
+    })
+    .constant('TEAM',{
+        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
+    });
---
+++ shanghaitech/app/client/views/login/loginCtrl.js
@@ -0,0 +1,58 @@
+angular.module('reg')
+  .controller('LoginCtrl', [
+    '$scope',
+    '$http',
+    '$state',
+    'settings',
+    'Utils',
+    'AuthService',
+    function($scope, $http, $state, settings, Utils, AuthService){
+
+      // Is registration open?
+      var Settings = settings.data;
+      $scope.regIsOpen = Utils.isRegOpen(Settings);
+
+      // Start state for login
+      $scope.loginState = 'login';
+
+      function onSuccess() {
+        $state.go('app.dashboard');
+      }
+
+      function onError(data){
+        $scope.error = data.message;
+      }
+
+      function resetError(){
+        $scope.error = null;
+      }
+
+      $scope.login = function(){
+        resetError();
+        AuthService.loginWithPassword(
+          $scope.email, $scope.password, onSuccess, onError);
+      };
+
+      $scope.register = function(){
+        resetError();
+        AuthService.register(
+          $scope.email, $scope.password, onSuccess, onError);
+      };
+
+      $scope.setLoginState = function(state) {
+        $scope.loginState = state;
+      };
+
+      $scope.sendResetEmail = function() {
+        var email = $scope.email;
+        AuthService.sendResetEmail(email);
+        sweetAlert({
+          title: "不用担心",
+          text: "密码重置邮件已发送到你的注册邮箱",
+          type: "success",
+          confirmButtonColor: "#e76482"
+        });
+      };
+
+    }
+  ]);
