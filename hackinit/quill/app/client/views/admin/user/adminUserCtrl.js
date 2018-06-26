4f39f4cb714d8821084c6657beb962f51189f7a3
yuchong-pan <panyuchong@gmail.com>
2018-06-25 20:41:42 -0700

    Added resume link to review portal of hack.init()

--- hackinit/quill/app/client/views/admin/user/adminUserCtrl.js
+++ hackinit/quill/app/client/views/admin/user/adminUserCtrl.js
@@ -10,6 +10,8 @@ angular.module('reg')
       // Populate the school dropdown
       populateSchools();
 
+      getUserResume();
+
       /**
        * TODO: JANK WARNING
        */
@@ -26,6 +28,17 @@ angular.module('reg')
               $scope.autoFilledSchool = true;
             }
 
+          });
+      }
+
+      function getUserResume() {
+        var id = $scope.selectedUser._id;
+        var prefix = 'upload/resume/hackinit/' + id + '_resume';
+        $http
+          .get('https://api.thehack.org.cn/s3/prefix/' + prefix)
+          .then(function(res) {
+            var url = res.data.result;
+            $scope.selectedUser.profile.resume = url;
           });
       }
 
