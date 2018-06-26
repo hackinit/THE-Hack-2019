451f2f0c529f88fbbd70a8ea3e18b1fce1c3f013
yuchong-pan <panyuchong@gmail.com>
2018-06-25 23:56:32 -0700

    Added console.log to hack.init() admin portal

--- hackinit/quill/app/client/views/admin/user/adminUserCtrl.js
+++ hackinit/quill/app/client/views/admin/user/adminUserCtrl.js
@@ -34,6 +34,7 @@ angular.module('reg')
       function getUserResume() {
         var id = $scope.selectedUser._id;
         var prefix = 'upload/resume/hackinit/' + id + '_resume';
+        console.log(prefix);
         $http
           .get('https://api.thehack.org.cn/s3/prefix/' + prefix)
           .then(function(res) {
