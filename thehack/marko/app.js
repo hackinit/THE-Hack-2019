d13ee8069722edbb90572871f095f9015afee162
yuchong-pan <panyuchong@gmail.com>
2018-07-17 02:23:27 -0700

    Added dummy QR code scanner page

--- thehack/marko/app.js
+++ thehack/marko/app.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app = express();
 
 //app.get(["/:lang", "*"], require("./src/pages/coming-soon"));
+app.get(["/checkin"], require("./src/pages/checkin"));
 app.get(["/:lang", "*"], require("./src/pages/home"));
 
 app.listen(8080);
