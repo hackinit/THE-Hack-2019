947c030a89d804d7c969f7e8ab8558990b1b7f68
yuchong-pan <panyuchong@gmail.com>
2018-07-18 01:38:11 -0700

    Added checkin confirm API endpoint to Django

--- django/checkin/urls.py
+++ django/checkin/urls.py
@@ -3,4 +3,5 @@ from checkin import views
 
 urlpatterns = [
     url(r'^info/(?P<group>hackinit|hackshanghai|shanghaitech)/(?P<identity>[a-zA-Z0-9]+)$', views.CheckInInformation.as_view()),
+    url(r'^confirm/(?P<group>hackinit|hackshanghai|shanghaitech)/(?P<identity>[a-zA-Z0-9]+)$', views.CheckInConfirm.as_view()),
 ]
