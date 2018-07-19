a2bb2e7da338605e7a3d0062bb0bb2f38f666876
yuchong-pan <panyuchong@gmail.com>
2018-07-18 23:55:43 -0700

    Removed redundant app.js

--- thehack/app.js
+++
@@ -1,9 +0,0 @@
-require("marko/express");
-require("marko/node-require").install();
-
-var express = require("express");
-var app = express();
-
-app.get(["/:lang", "*"], require("./src/pages/home"));
-
-app.listen(8080);
