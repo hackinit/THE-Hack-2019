a2b6ff3e11654fa212ae56edf4319667c0cc3d04
Ye Shu <shuye02@outlook.com>
2019-03-08 13:27:45 +0800

    Duplicate the 2018 static file to 2019

---
+++ 2019/static/js/hackinit/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2017, 12-1, 1, 8)}); // year, month, date, hour
+        });
+});		
+
