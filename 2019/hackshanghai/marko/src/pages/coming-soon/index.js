a455e92d1121e2eee4dc5568111588587c6ab2ea
yechs <chshu@protonmail.ch>
2019-03-12 09:14:26 +0800

    Remove hackinit and hackshanghai sites in 2019

--- 2019/hackshanghai/marko/src/pages/coming-soon/index.js
+++
@@ -1,7 +0,0 @@
-var dataAll = require("./langs.json");
-var template = require("./template.marko");
-var render = require("./../../scripts/render.js");
-
-module.exports = function(req, res) {
-    render(req, res, dataAll, template, "/");
-};
