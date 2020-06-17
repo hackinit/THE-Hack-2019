18b5904b2af55499bebca2ee59858880c24772ab
Ye Shu <chshu@protonmail.ch>
2020-06-18 00:01:01 +0800

    Bugfix: return when redirecting
    Return to avoid calling res.marko() when writing redirect to response
    This avoids the ERR_HTTP_HEADERS_SENT error

--- 2017/marko/src/pages/home/index.js
+++ 2017/marko/src/pages/home/index.js
@@ -3,17 +3,24 @@ var template = require("./template.marko");
 var supportLangs = ["zh"];
 
 function getLang(req, res) {
-    if (req.params.lang && supportLangs.indexOf(req.params.lang) != -1) {
-        return req.params.lang;
-    } else if (req.path === "/") {
-        return "en";
-    } else {
-        res.redirect("/");
-    }
+  if (req.params.lang && supportLangs.indexOf(req.params.lang) != -1) {
+    return req.params.lang;
+  } else if (req.path === "/") {
+    return "en";
+  } else {
+    return null;
+  }
 }
 
-module.exports = function(req, res) {
-    res.marko(template, {
-        lang: getLang(req, res)
-    });
+module.exports = function (req, res) {
+  var lang = getLang(req, res);
+  // If language is not supported, redirect to root (English)
+  if (lang === null) {
+    res.redirect("/");
+    // return to avoid res.marko() be executed
+    return;
+  }
+  res.marko(template, {
+    lang: lang,
+  });
 };
