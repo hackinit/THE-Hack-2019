604fbea82615705a8353e9b3fd2a4b7d03386a8c
Ye Shu <chshu@protonmail.ch>
2020-06-18 00:04:44 +0800

    Disable all pages except homepage

--- 2017/marko/app.js
+++ 2017/marko/app.js
@@ -4,15 +4,15 @@ require("marko/node-require").install();
 var express = require("express");
 var app = express();
 
-app.get(["/ambassador", "/:lang/ambassador"], require("./src/pages/ambassador"));
-app.get(["/login", "/:lang/login"], require("./src/pages/login"));
-app.get(["/reset", "/:lang/reset"], require("./src/pages/reset"));
-app.get(["/register", "/:lang/register"], require("./src/pages/register"));
-app.get(["/apply", "/:lang/apply"], require("./src/pages/apply"));
-app.get(["/dashboard", "/:lang/dashboard"], require("./src/pages/dashboard"));
-app.get(["/confirm", "/:lang/confirm"], require("./src/pages/confirm"));
-app.get(["/team", "/:lang/team"], require("./src/pages/team"));
-app.get(["/sponsor", "/:lang/sponsor"], require("./src/pages/sponsor"));
+// app.get(["/ambassador", "/:lang/ambassador"], require("./src/pages/ambassador"));
+// app.get(["/login", "/:lang/login"], require("./src/pages/login"));
+// app.get(["/reset", "/:lang/reset"], require("./src/pages/reset"));
+// app.get(["/register", "/:lang/register"], require("./src/pages/register"));
+// app.get(["/apply", "/:lang/apply"], require("./src/pages/apply"));
+// app.get(["/dashboard", "/:lang/dashboard"], require("./src/pages/dashboard"));
+// app.get(["/confirm", "/:lang/confirm"], require("./src/pages/confirm"));
+// app.get(["/team", "/:lang/team"], require("./src/pages/team"));
+// app.get(["/sponsor", "/:lang/sponsor"], require("./src/pages/sponsor"));
 app.get(["/:lang", "*"], require("./src/pages/home"));
 
 app.listen(8080);
--- 2018/thehack/marko/app.js
+++ 2018/thehack/marko/app.js
@@ -5,8 +5,8 @@ var express = require("express");
 var app = express();
 
 //app.get(["/:lang", "*"], require("./src/pages/coming-soon"));
-app.get(["/checkin"], require("./src/pages/checkin"));
-app.get(["/resumes", "/resumes/:lang"], require("./src/pages/resumes"));
+// app.get(["/checkin"], require("./src/pages/checkin"));
+// app.get(["/resumes", "/resumes/:lang"], require("./src/pages/resumes"));
 app.get(["/:lang", "*"], require("./src/pages/home"));
 
 app.listen(8080);
