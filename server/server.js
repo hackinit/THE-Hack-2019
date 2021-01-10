6dbe822c183430fc58de6d7c42ad7f5b45eaef04
Ye Shu <chshu@protonmail.ch>
2021-01-10 15:42:06 +0800

    Initialize server package that handles all sites

---
+++ server/server.js
@@ -0,0 +1,63 @@
+require("marko/node-require").install(); // Allow Node.js to require and load `.marko` files
+
+const express = require("express");
+const markoExpress = require("marko/express");
+
+const app = express();
+const port = 8080;
+
+var template = require("../2018/thehack/marko/src/pages/home/template.marko");
+
+app.use(markoExpress()); // enable res.marko(template, data)
+
+var isProduction = process.env.NODE_ENV === "production";
+
+// Configure lasso to control how JS/CSS/etc. is delivered to the browser
+require("lasso").configure({
+  plugins: [
+    "lasso-marko", // Allow Marko templates to be compiled and transported to the browser
+  ],
+  outputDir: __dirname + "/static", // Place all generated JS/CSS/etc. files into the "static" dir
+  bundlingEnabled: isProduction, // Only enable bundling in production
+  minify: isProduction, // Only minify JS and CSS code in production
+  fingerprintsEnabled: isProduction, // Only add fingerprints to URLs in production
+});
+
+app.use(require("lasso/middleware").serveStatic());
+
+// Routing based on domain name
+// Since all routes are "/:lang", this is relatively straightforward
+app.get(["/:lang", "*"], function (req, res) {
+  switch (req.headers.host) {
+    case "hackinit.org":
+      require("../2018/hackinit/marko/src/pages/home")(req, res);
+      break;
+    case "2017.hackinit.org":
+      require("../2017/marko/src/pages/home")(req, res);
+      break;
+    case "2018.hackshanghai.com":
+      require("../2018/hackshanghai/marko/src/pages/home")(req, res);
+      break;
+    // case not found
+    default:
+      // logging before fall-through to display thehack.org.cn
+      console.log(
+        "[",
+        new Date().toISOString(),
+        "] The request with following headers is recognized",
+        req.headers
+      );
+    case "2018.thehack.org.cn":
+      require("../2018/thehack/marko/src/pages/home")(req, res);
+      break;
+  }
+});
+
+
+app.listen(port, function () {
+  console.log("Server started! Try it out:\nhttp://localhost:" + port + "/");
+
+  if (process.send) {
+    process.send("online");
+  }
+});
